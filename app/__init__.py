5383d0b07646e4b7c0f6ac00d061aca6e2f3eb7e
BettK <kelvin.bett@student.moringaschool.com>
2021-08-24 06:10:51 +0300

    it will add view blogs function

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,9 @@ from flask_mail import Mail
 
 
 
+
 bootstrap =Bootstrap()
-db =SQLALchemy()
+db = SQLAlchemy()
 mail=Mail()
 
 
@@ -37,7 +38,7 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    return app
+#    return app
 
 # creating the app configurations
     app.config.from_object(config_options[config_name])
--- config.py
+++ config.py
@@ -1,18 +1,31 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ('SECRET KEY')
+  SQLALCHEMY_TRACK_MODIFICATIONS=True
+  SECRET_KEY = os.environ.get('SECRET_KEY')
 
+  #Email configurations
+  MAIL_SERVER = 'smtp.googlemail.com'
+  MAIL_PORT = 587
+  MAIL_USE_TLS = True
+  MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+  MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+  
+  UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+  #simple mde configurations
+  SIMPLEMDE_JS_IIFE = True
+  SIMPLEMDE_USE_CDN = True
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL")
-    pass
-
+  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+ 
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:read432q@localhost/blogs'
-    DEBUG = True
+  SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:read432q@localhost/blogs'
+  DEBUG = True
 
-config_options={
-    'development':DevConfig,
-    'production':ProdConfig
+
+config_options = {
+  'production':ProdConfig,
+  'development':DevConfig
 }
