d9a6f270895e24ca2f201be06153ab89cb6d65b3
BettK <kelvin.bett@student.moringaschool.com>
2021-08-25 20:39:00 +0300

    updated the quote url

--- app/requests.py
+++ app/requests.py
@@ -2,23 +2,22 @@
 from .models import Quotes
 import urllib.request, json
 
-quote_url ='https://quotes.ctormconsultancy.co.uk/random.json'
+QUOTE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
 
 def get_quote():
     """
     Function to get and consume http request and return a quote instance
     """
 
+    def get_quote():
     quotes_list=[]
-
-    new_quote_url=quote_url.format()
+    new_quote_url=QUOTE_URL.format()
     with urllib.request.urlopen(new_quote_url) as url:
-        get_data_url.read()
-        get_response=json.loads(get_data)
+        get_data=url.read()
+        get_response = json.loads(get_data)
         id=get_response['id']
         quote=str(get_response['quote'])
         author=str(get_response['author'])
-        new_list=[id,quote,author]
-        quote_list.append(new_list)
-
+        new_list=[id, quote, author]
+        quotes_list.append(new_list)
     return quotes_list[0]
--- manage.py
+++ manage.py
@@ -21,5 +21,3 @@ def make_shell_context():
 if __name__ =='__main__':
     manager.run()
 
-if __name__ =='__main__':
-    manager.run()
