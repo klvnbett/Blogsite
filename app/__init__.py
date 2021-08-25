96308843a18577f59ca9635c781d2f5f763fcb40
BettK <kelvin.bett@student.moringaschool.com>
2021-08-26 00:19:28 +0300

    added the image uploadset

--- app/__init__.py
+++ app/__init__.py
@@ -1,52 +1,42 @@
+from config import config_options
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
-from config import config_options
-from flask_login import LoginManager
+from flask_login import LoginManager, login_manager
 from flask_mail import Mail
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_sqlalchemy import SQLAlchemy
+from flask_simplemde import SimpleMDE
 
 
-
-
-bootstrap =Bootstrap()
 db = SQLAlchemy()
-mail=Mail()
+mail = Mail()
+simple = SimpleMDE()
+bootstrap = Bootstrap()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+photos = UploadSet('photos',IMAGES)
 
 
-login_manager=LoginManager()
-login_manager_session_protection='strong'
-login_manager.login_view='auth.login'
-
-
+#Create app instance
 def create_app(config_name):
-    app =Flask(__name__)
-
-
-# initializing flask extension
-    bootstrap.init_app(app)
-    db.init_app(app)
-    mail.init_app(app)
-    login_manager.init_app(app)
-
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix='/authenticate')
-
-#creating applications configurations
+    app = Flask(__name__)
     app.config.from_object(config_options[config_name])
-
-#registering Blue print
+    from .auth import auth as authentication_blueprint
     from .main import main as main_blueprint
+
+    #Registering bluenprints
+    app.register_blueprint(authentication_blueprint)
     app.register_blueprint(main_blueprint)
 
-#    return app
+    #Initializing flask extensions
+    login_manager.init_app(app)
+    db.init_app(app)
+    bootstrap.init_app(app)
+    mail.init_app(app)
 
-# creating the app configurations
-    app.config.from_object(config_options[config_name])
-
-#registering the Blue print
-
-    app.register_blueprint(main_blueprint)
+    #configure UploadsSet
+    configure_uploads(app,photos)
 
     return app
-
-
