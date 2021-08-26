10ec3c976f1dc3ac54688b7c61dee14401e03adf
BettK <kelvin.bett@student.moringaschool.com>
2021-08-26 15:58:11 +0300

    added an update to config

--- config.py
+++ config.py
@@ -18,7 +18,8 @@ class Config:
   SIMPLEMDE_USE_CDN = True
 class ProdConfig(Config):
   SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
- 
+  if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",1)
 
 class DevConfig(Config):
   SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:read432q@localhost/blogs'
--- manage.py
+++ manage.py
@@ -1,24 +1,23 @@
-from app import create_app,db
-from flask_script import Manager,Server
-from config import config_options
 from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager, Server
+
+from app import create_app, db
 from app.models import Blog, Comment, Quotes, User
+from config import config_options
 
-# creating app instance
 
-app =create_app('development')
+app = create_app('development')
 
-manager =Manager(app)
-manager.add_command('server',Server)
+manager = Manager(app)
+manager.add_command('server', Server)
 
 migrate=Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
 @manager.shell
-
 def make_shell_context():
-    return dict(app =app,db=db,User=User,Quotes=Quotes,Blog=Blog,Comment=Comment)
+    return dict(app=app,db=db,User=User,Quotes=Quotes,Blog=Blog,Comment=Comment)
 
-if __name__ =='__main__':
+
+if __name__ == '__main__':
     manager.run()
-
